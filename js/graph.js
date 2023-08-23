f9a629c6e15c2bd7cbba698b7f5b5d4ebecd024d
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-08-23 10:31:28 +0200

    edge added

--- js/graph.js
+++ js/graph.js
@@ -4,7 +4,8 @@ class Vertex {
         this.hopDist = Number.MAX_VALUE;
         this.weightDist = Number.MAX_VALUE;
         this._ID = id;
-        this._ADJACENCY = adjacency;
+        this._ADJACENCY = [...new Set(adjacency)];
+        this.parent = id; // technically empty, but unsure how to do so
     }
     get ID() {
         return this._ID;
@@ -14,6 +15,13 @@ class Vertex {
     }
 }
 ;
+class Edge {
+    constructor(source, target, weight) {
+        this._SOURCE = source;
+        this._TARGET = target;
+        this._WEIGHT = weight;
+    }
+}
 // class Graph {
 //     nodes: {}
 // };
--- ts/graph.ts
+++ ts/graph.ts
@@ -1,13 +1,15 @@
-class Vertex <T> { 
+class Vertex <T extends String | Number> { 
+
     private readonly _ID: T;
     private readonly _ADJACENCY: Array<T>;
+    private parent: T;
     private hopDist: Number = Number.MAX_VALUE;
     private weightDist: Number = Number.MAX_VALUE;
 
     constructor (id: T, adjacency: Array<T>) {
         this._ID = id;
-        this._ADJACENCY = adjacency;
-
+        this._ADJACENCY = [... new Set(adjacency)];
+        this.parent = id; // technically empty, but unsure how to do so
     }
 
     public get ID() {
@@ -17,7 +19,21 @@ class Vertex <T> {
     public get adjacency() {
         return this._ADJACENCY;
     }
+
 };
+
+class Edge <T extends String | Number> {
+
+    private readonly _SOURCE: T;
+    private readonly _TARGET: T;
+    private readonly _WEIGHT: Number;
+
+    constructor (source: T, target: T, weight: Number) {
+        this._SOURCE = source;
+        this._TARGET = target;
+        this._WEIGHT = weight;
+    }
+}
 
 // class Graph {
 //     nodes: {}
