c1ba58d7a6740c55e28a76293f7eaeb060cc0373
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-08-29 14:54:48 +0200

    uncommented main

--- ts/main.ts
+++ ts/main.ts
@@ -7,14 +7,14 @@ const ego: string = 'Napoleon';
 const data = JSONIO.rescaleEdges(JSONIO.readObjectsFromJSON(inputFileName + ".edges.json", "data"));
 console.log(data)
 
-// const graph = new Graph(data);
-// const distanceGraph = new DistanceGraph(graph);
-// distanceGraph.computeDijkstraDistances(ego);
-// const egoNodes = distanceGraph.getEgocentricDistances(ego);
-// const edgeList = distanceGraph.EdgeList;
-// const annotatedEdges = distanceGraph.getAnnotatedEdges(ego);
-// JSONIO.writeObjectsToJSON(egoNodes, inputFileName + ".nodes.json", "./data");
-// JSONIO.writeObjectsToJSON(annotatedEdges, inputFileName + ".annotated.edges.json", "./data");
-// console.log(egoNodes);
-// console.log(edgeList);
-// console.log(annotatedEdges);
+const graph = new Graph(data);
+const distanceGraph = new DistanceGraph(graph);
+distanceGraph.computeDijkstraDistances(ego);
+const egoNodes = distanceGraph.getEgocentricDistances(ego);
+const edgeList = distanceGraph.EdgeList;
+const annotatedEdges = distanceGraph.getAnnotatedEdges(ego);
+JSONIO.writeObjectsToJSON(egoNodes, inputFileName + ".nodes.json", "./data");
+JSONIO.writeObjectsToJSON(annotatedEdges, inputFileName + ".annotated.edges.json", "./data");
+console.log(egoNodes);
+console.log(edgeList);
+console.log(annotatedEdges);
