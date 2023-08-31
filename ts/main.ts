0da4cd5f2205e382b91e0096875be42286456532
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-08-31 16:43:57 +0200

    better file naming

--- ts/main.ts
+++ ts/main.ts
@@ -3,14 +3,13 @@ import { Graph } from "./Graph";
 import { DistanceGraph } from "./DistanceGraph";
 
 const inputFileName: string = 'miserables';
-const ego: string = 'Valjean';
+const ego: string = 'Javert';
 const data = JSONIO.rescaleEdges(JSONIO.readObjectsFromJSON(inputFileName + ".edges.json", "data"));
 
 const graph = new Graph(data);
 const distanceGraph = new DistanceGraph(graph);
 distanceGraph.computeDijkstraDistances(ego);
 const egoNodes = distanceGraph.getEgocentricDistances(ego);
-const edgeList = distanceGraph.EdgeList;
 const annotatedEdges = distanceGraph.getAnnotatedEdges(ego);
-JSONIO.writeObjectsToJSON(egoNodes, inputFileName + ".nodes.json", "./data");
-JSONIO.writeObjectsToJSON(annotatedEdges, inputFileName + ".annotated.edges.json", "./data");
+JSONIO.writeObjectsToJSON(egoNodes, inputFileName + "." + ego + ".nodes.json", "./data");
+JSONIO.writeObjectsToJSON(annotatedEdges, inputFileName + "." + ego + ".edges.json", "./data");
