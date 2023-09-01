b043e9616c1e14e6ff8432427d1ca3de68413329
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-09-01 18:33:16 +0200

    rerun

--- ts/main.ts
+++ ts/main.ts
@@ -2,8 +2,8 @@ import { JSONIO } from "./JSONIO";
 import { Graph } from "./Graph";
 import { DistanceGraph } from "./DistanceGraph";
 
-const inputFileName: string = 'mouse.visual_cortex_1';
-const ego: string = '1';
+const inputFileName: string = 'miserables';
+const ego: string = 'Valjean';
 const data = JSONIO.rescaleEdges(JSONIO.readObjectsFromJSON(inputFileName + ".edges.json", "data"));
 
 const graph = new Graph(data);
